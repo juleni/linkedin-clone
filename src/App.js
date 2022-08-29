5281db5cc84bd4a89e622260e9ba355a774d7e10
juleni <69912179+juleni@users.noreply.github.com>
2022-08-29 21:29:09 +0200

    Added Login component and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return 
-  <div className="App">
-    Linkedin clone
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
