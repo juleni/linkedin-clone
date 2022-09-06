ecd159b7819251c9f8453173417c07fe4126c202
juleni <69912179+juleni@users.noreply.github.com>
2022-09-06 17:02:19 +0200

    AddedSign-In and Section

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import styled from 'styled-components';
 
 const Login = () => {
   return (
@@ -11,8 +10,14 @@ const Login = () => {
         </a>
         <div>
           <Join>Join now</Join>
+          <SignIn>Sign in</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+        </Hero>
+      </Section>
     </Container>
   );
 };
@@ -41,7 +46,7 @@ const Nav = styled.nav`
   }
 `;
 
-const Join = styled.nav`
+const Join = styled.a`
   font-size: 16px;
   padding: 10px 12px;
   text-decoration: none;
@@ -53,6 +58,62 @@ const Join = styled.nav`
     background-color: rgba(0, 0, 0, 0.08);
     color: rgba(0, 0, 0, 0.9);
     text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
   }
 `;
 
